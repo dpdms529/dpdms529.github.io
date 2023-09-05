415068432832f0ccfaf02f0d8f6503d7325b120e
dpdms529 <dpdms529@users.noreply.github.com>
2023-09-05 15:04:36 +0000

    jekyll build from Action 1a7ba5923807307d26ab1457851cdd6b099c87bc

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,67 @@
+var store = [{
+        "title": "[스프링 핵심 원리 기본편] 1. 객체 지향 설계와 스프링",
+        "excerpt":"스프링 핵심 원리 - 기본편 - 인프런 | 강의 스프링 입문자가 예제를 만들어가면서 스프링의 핵심 원리를 이해하고, 스프링 기본기를 확실히 다질 수 있습니다., - 강의 소개 | 인프런 www.inflearn.com 자바 진영의 추운 겨울과 스프링의 탄생 EJB(Enterprise Java Beans) 2000년대 초반 자바 표준 기술 Entity Bean : ORM 기술 이론이 좋고,...","categories": ["Lectures","스프링 핵심 원리 기본편"],
+        "tags": ["Lectures","스프링 핵심 원리 기본편","Spring"],
+        "url": "/lectures/%EC%8A%A4%ED%94%84%EB%A7%81-%ED%95%B5%EC%8B%AC-%EC%9B%90%EB%A6%AC-%EA%B8%B0%EB%B3%B8%ED%8E%B8/1-%EA%B0%9D%EC%B2%B4-%EC%A7%80%ED%96%A5-%EC%84%A4%EA%B3%84%EC%99%80-%EC%8A%A4%ED%94%84%EB%A7%81/",
+        "teaser": null
+      },{
+        "title": "[스프링 핵심 원리 기본편] 2. 예제 만들기",
+        "excerpt":"스프링 핵심 원리 - 기본편 - 인프런 | 강의 스프링 입문자가 예제를 만들어가면서 스프링의 핵심 원리를 이해하고, 스프링 기본기를 확실히 다질 수 있습니다., - 강의 소개 | 인프런 www.inflearn.com 프로젝트 생성 Project : Gradle - Groovy Language : Java Spring Boot : SNAPSHOT, RC, M 등이 붙지 않은 버전 중...","categories": ["Lectures","스프링 핵심 원리 기본편"],
+        "tags": ["Lectures","스프링 핵심 원리 기본편","Spring"],
+        "url": "/lectures/%EC%8A%A4%ED%94%84%EB%A7%81-%ED%95%B5%EC%8B%AC-%EC%9B%90%EB%A6%AC-%EA%B8%B0%EB%B3%B8%ED%8E%B8/2-%EC%98%88%EC%A0%9C-%EB%A7%8C%EB%93%A4%EA%B8%B0/",
+        "teaser": null
+      },{
+        "title": "[스프링 핵심 원리 기본편] 3. 객체 지향 원리 적용",
+        "excerpt":"스프링 핵심 원리 - 기본편 - 인프런 | 강의 스프링 입문자가 예제를 만들어가면서 스프링의 핵심 원리를 이해하고, 스프링 기본기를 확실히 다질 수 있습니다., - 강의 소개 | 인프런 www.inflearn.com 정률 할인 기능 추가 코드 문제점 할인 정책을 변경하려면 클라이언트인 OrderServiceImpl 코드를 고쳐야 함 Before : private final DiscountPolicy discountPolicy =...","categories": ["Lectures","스프링 핵심 원리 기본편"],
+        "tags": ["Lectures","스프링 핵심 원리 기본편","Spring"],
+        "url": "/lectures/%EC%8A%A4%ED%94%84%EB%A7%81-%ED%95%B5%EC%8B%AC-%EC%9B%90%EB%A6%AC-%EA%B8%B0%EB%B3%B8%ED%8E%B8/3-%EA%B0%9D%EC%B2%B4-%EC%A7%80%ED%96%A5-%EC%9B%90%EB%A6%AC-%EC%A0%81%EC%9A%A9/",
+        "teaser": null
+      },{
+        "title": "[스프링 핵심 원리 기본편] 4. 스프링 컨테이너와 스프링 빈",
+        "excerpt":"스프링 핵심 원리 - 기본편 - 인프런 | 강의 스프링 입문자가 예제를 만들어가면서 스프링의 핵심 원리를 이해하고, 스프링 기본기를 확실히 다질 수 있습니다., - 강의 소개 | 인프런 www.inflearn.com 스프링 컨테이너 //스프링 컨테이너 생성 ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); ApplicationContext는 인터페이스로 구현체에 따라 XML 또는 애노테이션 기반의 자바 설정 클래스로...","categories": ["Lectures","스프링 핵심 원리 기본편"],
+        "tags": ["Lectures","스프링 핵심 원리 기본편","Spring"],
+        "url": "/lectures/%EC%8A%A4%ED%94%84%EB%A7%81-%ED%95%B5%EC%8B%AC-%EC%9B%90%EB%A6%AC-%EA%B8%B0%EB%B3%B8%ED%8E%B8/4-%EC%8A%A4%ED%94%84%EB%A7%81-%EC%BB%A8%ED%85%8C%EC%9D%B4%EB%84%88%EC%99%80-%EC%8A%A4%ED%94%84%EB%A7%81-%EB%B9%88/",
+        "teaser": null
+      },{
+        "title": "[스프링 핵심 원리 기본편] 5. 싱글톤 컨테이너",
+        "excerpt":"스프링 핵심 원리 - 기본편 - 인프런 | 강의 스프링 입문자가 예제를 만들어가면서 스프링의 핵심 원리를 이해하고, 스프링 기본기를 확실히 다질 수 있습니다., - 강의 소개 | 인프런 www.inflearn.com 웹 애플리케이션과 싱글톤 순수한 DI 컨테이너 코드 웹 애플리케이션으 보통 여러 고객이 동시에 요청 스프링 없는 순수한 DI 컨테이너는 요청을 할...","categories": ["Lectures","스프링 핵심 원리 기본편"],
+        "tags": ["Lectures","스프링 핵심 원리 기본편","Spring"],
+        "url": "/lectures/%EC%8A%A4%ED%94%84%EB%A7%81-%ED%95%B5%EC%8B%AC-%EC%9B%90%EB%A6%AC-%EA%B8%B0%EB%B3%B8%ED%8E%B8/5-%EC%8B%B1%EA%B8%80%ED%86%A4-%EC%BB%A8%ED%85%8C%EC%9D%B4%EB%84%88/",
+        "teaser": null
+      },{
+        "title": "[스프링 핵심 원리 기본편] 6. 컴포넌트 스캔",
+        "excerpt":"스프링 핵심 원리 - 기본편 - 인프런 | 강의 스프링 입문자가 예제를 만들어가면서 스프링의 핵심 원리를 이해하고, 스프링 기본기를 확실히 다질 수 있습니다., - 강의 소개 | 인프런 www.inflearn.com 컴포넌트 스캔과 의존관계 자동 주입 코드 스프링은 설정 정보가 없어도 자동으로 스프링 빈을 등록하는 컴포넌트 스캔이라는 기능 제공 의존관계를 자동으로 주입하는...","categories": ["Lectures","스프링 핵심 원리 기본편"],
+        "tags": ["Lectures","스프링 핵심 원리 기본편","Spring"],
+        "url": "/lectures/%EC%8A%A4%ED%94%84%EB%A7%81-%ED%95%B5%EC%8B%AC-%EC%9B%90%EB%A6%AC-%EA%B8%B0%EB%B3%B8%ED%8E%B8/6-%EC%BB%B4%ED%8F%AC%EB%84%8C%ED%8A%B8-%EC%8A%A4%EC%BA%94/",
+        "teaser": null
+      },{
+        "title": "[스프링 핵심 원리 기본편] 7. 의존관계 자동 주입",
+        "excerpt":"스프링 핵심 원리 - 기본편 - 인프런 | 강의 스프링 입문자가 예제를 만들어가면서 스프링의 핵심 원리를 이해하고, 스프링 기본기를 확실히 다질 수 있습니다., - 강의 소개 | 인프런 www.inflearn.com 다양한 의존관계 주입 방법 생성자 주입 생성자를 통해 의존 관계를 주입 받는 방법 생성자 호출 시점에 딱 1번만 호출되는 것이 보장...","categories": ["Lectures","스프링 핵심 원리 기본편"],
+        "tags": ["Lectures","스프링 핵심 원리 기본편","Spring"],
+        "url": "/lectures/%EC%8A%A4%ED%94%84%EB%A7%81-%ED%95%B5%EC%8B%AC-%EC%9B%90%EB%A6%AC-%EA%B8%B0%EB%B3%B8%ED%8E%B8/7-%EC%9D%98%EC%A1%B4%EA%B4%80%EA%B3%84-%EC%9E%90%EB%8F%99-%EC%A3%BC%EC%9E%85/",
+        "teaser": null
+      },{
+        "title": "[Jekyll 블로그] Jekyll 블로그 생성",
+        "excerpt":"Jekyll 블로그 시작! 블로그를 한동안 방치했었는데 시간도 생겼고, 다시 잘 운영해보고 싶어서 싹 밀고 새로 만들었다. 한 번 했었으니깐 다시 만들면 금방 만들 줄 았았는데 생각보다 시간이 오래 걸렸다. 혹시 또 jekyll 블로그를 새로 만들고 싶어질지도 모르니깐 블로그 생성 과정을 정리해보려고 한다. 설치 Jekyll on Windows While Windows is not...","categories": ["Experiences","Jekyll 블로그"],
+        "tags": ["Experiences","Jekyll 블로그"],
+        "url": "/experiences/jekyll-%EB%B8%94%EB%A1%9C%EA%B7%B8/Jekyll-%EB%B8%94%EB%A1%9C%EA%B7%B8-%EC%83%9D%EC%84%B1/",
+        "teaser": null
+      },{
+        "title": "[Jekyll 블로그] Github Actions로 배포",
+        "excerpt":"Github Actions 이전 포스트에서 설명했다시피 블로그의 변경사항을 자동 배포하기 위해 Github Actions를 사용해보려한다. Actions 탭에 들어가서 set up a workflow youself를 클릭한다. 파일명을 github-pages로 하고 내용을 다음과 같이 작성한다. name: Build and deploy Jekyll site to GitHub Pages on: push: branches: - main jobs: github-pages: runs-on: ubuntu-latest steps: - uses:...","categories": ["Experiences","Jekyll 블로그"],
+        "tags": ["Experiences","Jekyll 블로그"],
+        "url": "/experiences/jekyll-%EB%B8%94%EB%A1%9C%EA%B7%B8/Github-Actions%EB%A1%9C-%EB%B0%B0%ED%8F%AC/",
+        "teaser": null
+      },{
+        "title": "[스프링 핵심 원리 기본편] 8. 빈 생명주기 콜백",
+        "excerpt":"스프링 핵심 원리 - 기본편 - 인프런 | 강의 스프링 입문자가 예제를 만들어가면서 스프링의 핵심 원리를 이해하고, 스프링 기본기를 확실히 다질 수 있습니다., - 강의 소개 | 인프런 www.inflearn.com 빈 생명주기 콜백 시작 https://github.com/dpdms529/SpringCore/commit/0f3d1de78f5b14aa2293c79d5f7e735dc264054e 데이터베이스 커넥션 풀이나, 네티워크 소켓처럼 애플리케이션 시작 시점에 필요한 연결을 미리 해두고, 애플리케이션 종료 시점에 연결을...","categories": ["Lectures","스프링 핵심 원리 기본편"],
+        "tags": ["Lectures","스프링 핵심 원리 기본편","Spring"],
+        "url": "/lectures/%EC%8A%A4%ED%94%84%EB%A7%81-%ED%95%B5%EC%8B%AC-%EC%9B%90%EB%A6%AC-%EA%B8%B0%EB%B3%B8%ED%8E%B8/8-%EB%B9%88-%EC%83%9D%EB%AA%85%EC%A3%BC%EA%B8%B0-%EC%BD%9C%EB%B0%B1/",
+        "teaser": null
+      },{
+        "title": "[스프링 핵심 원리 기본편] 9. 빈 스코프",
+        "excerpt":"스프링 핵심 원리 - 기본편 - 인프런 | 강의 스프링 입문자가 예제를 만들어가면서 스프링의 핵심 원리를 이해하고, 스프링 기본기를 확실히 다질 수 있습니다., - 강의 소개 | 인프런 www.inflearn.com 빈 스코프란? 스프링 빈이 스프링 컨테이너의 시작과 함께 생성되어 스프링 컨테이너가 종료될 때까지 유지되는 이유는 스프링 빈이 기본적으로 싱글톤 스코프로 생성되기...","categories": ["Lectures","스프링 핵심 원리 기본편"],
+        "tags": ["Lectures","스프링 핵심 원리 기본편","Spring"],
+        "url": "/lectures/%EC%8A%A4%ED%94%84%EB%A7%81-%ED%95%B5%EC%8B%AC-%EC%9B%90%EB%A6%AC-%EA%B8%B0%EB%B3%B8%ED%8E%B8/9-%EB%B9%88-%EC%8A%A4%EC%BD%94%ED%94%84/",
+        "teaser": null
+      }]
